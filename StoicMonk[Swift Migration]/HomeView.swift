e6775f4b370c5d315749d49d950c22dc2a08098a
Jayanta Adhikary <adhikaryjayanta@outlook.com>
2023-09-15 13:30:08 +0530

    api call

--- StoicMonk[Swift Migration]/HomeView.swift
+++ StoicMonk[Swift Migration]/HomeView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var author: String?
+    @State var quote: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            Text(quote ?? "Not Found")
+            Text(author ?? "Not Found")
+        }
+        .onAppear{
+            Task {
+                await apiCall()
+            }
+        }
+        
+    }
+    
+    func apiCall() async{
+        if let url = URL(string: "https://stoic-quotes.com/api/quote") {
+            let request = URLRequest(url: url)
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                    
+                let decoder = JSONDecoder()
+                
+                do {
+                    let quote = try decoder.decode(Quote.self, from: data)
+                    
+                    self.quote =  quote.text ?? "Not Found"
+                    self.author = quote.author ?? "Not Found"
+                    
+//                    print(quote.author ?? "Not Found")
+//                    print(quote.text ?? "Not Found")
+                    
+                } catch {
+                    print(error)
+                }
+                
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-}
+//struct HomeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView()
+//    }
+//}
---
+++ StoicMonk[Swift Migration]/Model/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  StoicMonk[Swift Migration]
+//
+//  Created by Jayanta Adhikary on 15/09/23.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    var author: String?
+    var text: String?
+}
