6e98f35b85fac1a0b478f2e1a14e042ea9b05efc
Jayanta Adhikary <adhikaryjayanta@outlook.com>
2023-09-16 00:00:57 +0530

    Side drawer initial

--- StoicMonk[Swift Migration]/HomeView.swift
+++ StoicMonk[Swift Migration]/HomeView.swift
@@ -11,57 +11,89 @@ struct HomeView: View {
     
     @State var author: String?
     @State var quote: String?
+    @State var showMenu: Bool = false
     
     var body: some View {
-        VStack{
-            Text("Stoic Quotes")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color(red: 0.043137254901960784, green: 0.29411764705882354, blue: 0.11372549019607843))
+        NavigationView{
             
-            
-            VStack {
-                Text(quote ?? "Not Found")
-                    .font(.system(size: 18))
-                    .padding(.horizontal, 25)
-                    .padding(.top, 15)
-                HStack {
-                    Spacer()
-                    Text("- \(author ?? "Not Found")")
-                        .font(.system(size: 15))
-                        .padding(15)
+            ZStack {
+                VStack{
+                    Text("Stoic Quotes")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.043137254901960784, green: 0.29411764705882354, blue: 0.11372549019607843))
+                    
+                    
+                    VStack {
+                        Text(quote ?? "Not Found")
+                            .font(.system(size: 18))
+                            .padding(.horizontal, 25)
+                            .padding(.top, 15)
+                        HStack {
+                            Spacer()
+                            Text("- \(author ?? "Not Found")")
+                                .font(.system(size: 15))
+                                .padding(15)
+                        }
+                        
+                    }
+                    .foregroundColor(Color.white)
+                    .background(Color(UIColor(red: 16/255, green: 109/255, blue: 96/255, alpha: 1.0)))
+                    .cornerRadius(10)
+                    .padding(30)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    
+                    Button {
+                        Task {
+                            await apiCall()
+                        }
+                    } label: {
+                        Text("Refresh")
+                            .font(.system(size: 16))
+                            .padding(.horizontal, 50)
+                            .padding(.vertical, 25)
+                            .cornerRadius(12)
+                            .frame(height: 24)
+                            .foregroundColor(.white)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red: 0.043137254901960784, green: 0.29411764705882354, blue: 0.11372549019607843))
+                    
+                
+                    
                 }
                 
+                GeometryReader { geometry in
+                    SideMenuView()
+                        .offset(x: showMenu ? 0 : -geometry.size.width) // Slide in from the left
+                        .animation(.easeInOut(duration: 0.3)) // Add easeInOut animation with a duration
+                }
             }
-            .foregroundColor(Color.white)
-            .background(Color(UIColor(red: 16/255, green: 109/255, blue: 96/255, alpha: 1.0)))
-            .cornerRadius(10)
-            .padding(30)
-            .frame(maxWidth: .infinity, alignment: .center)
-            
-            Button {
+            .onAppear{
                 Task {
                     await apiCall()
                 }
-            } label: {
-                Text("Refresh")
-                    .font(.system(size: 16))
-                    .padding(.horizontal, 50)
-                    .padding(.vertical, 25)
-                    .cornerRadius(12)
-                    .frame(height: 24)
-                    .foregroundColor(.white)
             }
-            .buttonStyle(.borderedProminent)
-            .tint(Color(red: 0.043137254901960784, green: 0.29411764705882354, blue: 0.11372549019607843))
-            
-            
-        }
-        .onAppear{
-            Task {
-                await apiCall()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        
+                        self.showMenu.toggle()
+                    } label: {
+                        Image(systemName: "text.justify.left")
+                            .font(.subheadline)
+                            .foregroundColor(Color(red: 0.043137254901960784, green: 0.29411764705882354, blue: 0.11372549019607843))
+                    }
+                    
+                    
+                    
+                }
+                
+                
             }
+           
         }
+        
         
     }
     
---
+++ StoicMonk[Swift Migration]/SideMenuView.swift
@@ -0,0 +1,47 @@
+//
+//  SideMenuView.swift
+//  StoicMonk[Swift Migration]
+//
+//  Created by Jayanta Adhikary on 15/09/23.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        VStack {
+            
+            NavigationLink(destination: Text("Item 1")) {
+                Text("Item 1")
+                    .font(.subheadline)
+                    .padding(.vertical, 10)
+            }
+            
+            NavigationLink(destination: Text("Item 2")) {
+                Text("Item 2")
+                    .font(.subheadline)
+                    .padding(.vertical, 10)
+            }
+            
+            
+            Spacer()
+        }
+        .padding(.horizontal, 72)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.9568627450980393, green: 0.984313725490196, blue: 0.9803921568627451)/*@END_MENU_TOKEN@*/)
+        .edgesIgnoringSafeArea(.bottom)
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
